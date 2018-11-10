1b984323f8e362d61d5f9f1f544fc52aff481eb1
amberyuy <43117026+amberyuy@users.noreply.github.com>
2018-11-10 18:22:22 -0500

    Add files via upload

---
+++ Abstractive_Summary_2/main_txt.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+import pandas as pd
+from keras_text_summarization.library.seq2seq import Seq2SeqSummarizer
+import numpy as np
+
+np.random.seed(42)
+data_dir_path = './demo/data' # refers to the demo/data folder
+model_dir_path = './demo/models' # refers to the demo/models folder
+
+with open('predict.txt', 'r') as myfile:
+  X = myfile.read()
+
+
+
+config = np.load(Seq2SeqSummarizer.get_config_file_path(model_dir_path=model_dir_path)).item()
+
+summarizer = Seq2SeqSummarizer(config)
+summarizer.load_weights(weight_file_path=Seq2SeqSummarizer.get_weight_file_path(model_dir_path=model_dir_path))
+
+x = X
+#actual_headline = Y
+headline = summarizer.summarize(X)
+print("\n=======================================================")
+print('Article: ', x)
+print("Output------------------------------------------------")
+print('Generated Headline: ', headline)
+#print('Original Headline: ', actual_headline)
+
+
+file = open('result.text','w')
+file.write(headline)
