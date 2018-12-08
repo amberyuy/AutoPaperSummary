e10afdd1455fdcc1bcc11fac28009f838fdc180a
Bonniesty <43017800+Bonniesty@users.noreply.github.com>
2018-12-08 10:05:34 -0500

    Update index.php

--- Sites/index.php
+++ Sites/index.php
@@ -10,7 +10,7 @@
 <head></head><body><div style="margin-left:350px;margin-top:150px;font-size:80px;color:#5151A2">Paper Summary</div>
 
 
-<marquee bgcolor="#355bb7" loop="-1" scrollamount="2" width="100%" >Hi This Auto Paper Summary Group - Welcome to our website!!</marquee>
+<marquee bgcolor="#ff69b4" loop="-1" scrollamount="2" width="100%" >Hi This Auto Paper Summary Group - Welcome to our website!!</marquee>
 
 <form method="POST" enctype="multipart/form-data"action="upload-file.php">
 
