0ac6aca29332145418538ea4e3093f0af378a71d
Bonniesty <43017800+Bonniesty@users.noreply.github.com>
2018-12-08 11:10:32 -0500

    Update index.php

--- Sites/index.php
+++ Sites/index.php
@@ -4,20 +4,31 @@
 <html>
 <head>
 <title>Paper Summary</title>
+<style type="text/css">
+
+
+.auto-style2 {
+
+	margin-left: 450px;font-size:36px;color:#FFC1E0
+}
+
+</style>
 </head>
 <body background = "back.jpg">
+
+<marquee bgcolor="#ffffba" loop="-1" scrollamount="2" width="100%"  style="margin-top:60px;" >Hi This is Auto Paper Summary Group - Welcome to our website!!</marquee>
 
 <head></head><body><div style="margin-left:350px;margin-top:150px;font-size:80px;color:#5151A2">Paper Summary</div>
 
 
-<marquee bgcolor="#ff69b4" loop="-1" scrollamount="2" width="100%" >Hi This Auto Paper Summary Group - Welcome to our website!!</marquee>
 
+<br><br>
 <form method="POST" enctype="multipart/form-data"action="upload-file.php">
 
 <div>
-<label for="file" style="margin-left: 450px;font-size:20px;color:#FFC1E0">Choose file to upload</label>
-<input type="file" name="file" style="margin-left:450px" accept=".txt">
-<input type="submit" name="submit" value="Upload" />
+<label for="file" class="auto-style2"><strong><span>Choose file to upload </span></strong></label>
+<input type="file" name="file" style="background-color: #ff748c; margin-left:450px" accept=".txt">
+<input type="submit" name="submit" value="Upload" style=" background-color: #ff748c;" />
 </div>
 </form>
 
