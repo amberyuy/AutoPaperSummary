81f6e8b2e1a2031b8499b7e2171ec8f76fa6954d
jyueling <43077618+jyueling@users.noreply.github.com>
2018-12-08 13:28:12 -0500

    Add files via upload

--- Sites/index.php
+++ Sites/index.php
@@ -4,31 +4,22 @@
 <html>
 <head>
 <title>Paper Summary</title>
-<style type="text/css">
-
-
-.auto-style2 {
-
-	margin-left: 450px;font-size:36px;color:#FFC1E0
-}
-
-</style>
 </head>
 <body background = "back.jpg">
-
-<marquee bgcolor="#ffffba" loop="-1" scrollamount="2" width="100%"  style="margin-top:60px;" >Hi This is Auto Paper Summary Group - Welcome to our website!!</marquee>
 
 <head></head><body><div style="margin-left:350px;margin-top:150px;font-size:80px;color:#5151A2">Paper Summary</div>
 
 
 
-<br><br>
+
 <form method="POST" enctype="multipart/form-data"action="upload-file.php">
 
 <div>
-<label for="file" class="auto-style2"><strong><span>Choose file to upload </span></strong></label>
-<input type="file" name="file" style="background-color: #ff748c; margin-left:450px" accept=".txt">
-<input type="submit" name="submit" value="Upload" style=" background-color: #ff748c;" />
+<br>
+<label for="file" style="margin-left: 450px;font-size:30px;color:#FFC1E0"><strong><span>Choose file to upload</strong></span></label>
+<br>
+<input type="file" name="file" style="margin-left:450px" accept=".txt">
+<input type="submit" name="submit" value="Upload" />
 </div>
 </form>
 
