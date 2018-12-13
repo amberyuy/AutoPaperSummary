09db6a019db090dbda65e101fc3f334101de1cbc
yuxuan liu <43069945+crazieemma@users.noreply.github.com>
2018-12-13 10:23:58 -0500

    Update train.py

--- Abstractive_Summary_2/train.py
+++ Abstractive_Summary_2/train.py
@@ -8,6 +8,9 @@ import os
 import pandas as pd
 import numpy as np
 from collections import Counter
+from matplotlib import pyplot as plt
+import numpy as np
+import itertools
 
 Load_Weight = False
 
@@ -81,6 +84,101 @@ def Prepro(X, Y, seqin_max=None, seqtar_max=None):
     config['maxtarseq'] = maxtarseq
 
     return config
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    See full source and example:
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+def most_informative_feature_for_binary_classification(vectorizer, classifier, n=100):
+    class_labels = classifier.classes_
+    feature_names = vectorizer.get_feature_names()
+    topn_class1 = sorted(zip(classifier.coef_[0], feature_names))[:n]
+    topn_class2 = sorted(zip(classifier.coef_[0], feature_names))[-n:]
+
+    for coef, feat in topn_class1:
+        print(class_labels[0], coef, feat)
+
+    print()
+
+    for coef, feat in reversed(topn_class2):
+        print(class_labels[1], coef, feat)
+
+
+def plot_history_2win(history):
+    plt.subplot(211)
+    plt.title('Accuracy')
+    plt.plot(history.history['acc'], color='g', label='Train')
+    plt.plot(history.history['val_acc'], color='b', label='Validation')
+    plt.legend(loc='best')
+
+    plt.subplot(212)
+    plt.title('Loss')
+    plt.plot(history.history['loss'], color='g', label='Train')
+    plt.plot(history.history['val_loss'], color='b', label='Validation')
+    plt.legend(loc='best')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def create_history(history, model_name, metrics=None):
+    plt.title('Accuracy and Loss (' + model_name + ')')
+    if metrics is None:
+        metrics = {'acc', 'loss'}
+    if 'acc' in metrics:
+        plt.plot(history.history['acc'], color='g', label='Train Accuracy')
+        plt.plot(history.history['val_acc'], color='b', label='Validation Accuracy')
+    if 'loss' in metrics:
+        plt.plot(history.history['loss'], color='r', label='Train Loss')
+        plt.plot(history.history['val_loss'], color='m', label='Validation Loss')
+    plt.legend(loc='best')
+
+    plt.tight_layout()
+
+
+def plot_history(history, model_name):
+    create_history_plot(history, model_name)
+    plt.show()
+
+
+def save_plot_history(history, model_name, file_path, metrics=None):
+    if metrics is None:
+        metrics = {'acc', 'loss'}
+    create_history(history, model_name, metrics)
+    plt.savefig(file_path)
+
 
 
 class Seq_to_Seq():
@@ -266,6 +364,8 @@ class Seq_to_Seq():
 
             states_value = [h, c]
         return target_text.strip()
+    
+ 
 
 
 def main():
@@ -321,6 +421,15 @@ def main():
     print('testing size: ', len(Xtest))
 
     print('start fitting ...')
+    
+    history = summarizer.fit(Xtrain, Ytrain, Xtest, Ytest, epochs=100)
+    
+    image_path = '/model/report' # you should change this path
+    history_plot_path = image_path + '/' + seq_to_seq.model_name + '.png'
+    if LOAD_EXISTING_WEIGHTS:
+        history_plot_path = image_path + '/' + seq_to_seq.model_name + '-v' + str(summarizer.version) + '.png'
+    
+    save_plot_history(history, seq_to_seq.model_name, history_plot_path, metrics={'loss', 'acc'})
 
 
 if __name__ == '__main__':
